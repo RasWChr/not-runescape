7bbb0b879a2b95a606daeae0940758a7e62dc3ba
RasWChr <r.west.chr@gmail.com>
2026-08-31 12:55:52 +0200

    added gold

--- NotRunescape/Player.cs
+++ NotRunescape/Player.cs
@@ -4,7 +4,10 @@ public class Player
 {
     public int CurrentHp { get; set; } = 35;
     public int MaxHp { get; set; } = 35;
-    public int Gold { get; set; } = 50;
+    public int Gold { get; set; } = StartingGold;
+
+    private const int StartingGold = 100;
+    
     public Dictionary<string, int> Inventory { get; set; } = new()
     {
         { "Lobster", 3 },
